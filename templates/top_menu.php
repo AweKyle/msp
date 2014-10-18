185ee41cb940f86bd9c52b686ec63e2a5dfeacfa
Kyle <kyle.voronin@gmail.com>
2014-10-18 15:41:14 +0400

    upd #3

--- templates/top_menu.php
+++ templates/top_menu.php
@@ -1,10 +1,57 @@
 <!DOCTYPE html>
 <html>
+<style type="text/css">
+	.top_ul {
+		list-style: none;
+		display: inline-flex;
+		float: right;
+		margin-top: 0px;
+	}
+	.top_li {
+		padding-top: 11px;
+		padding-right: 35px;
+	}
+	.top_li:last-child {
+		border-left: 1px solid #b5d7d6;
+		margin-left: 20px;
+		padding: 0px;
+	}
+	.lock {
+		width: 40px;
+		margin-left: 5px;
+	}
+	.lock:hover {
+		background-color: cadetblue;
+		border-radius: 7px;
+	}
+	.inp_search {
+		border-radius: 4px;
+		height: 18px;
+		font-style: italic;
+
+	}
+	.select-box {
+		width: 155px;
+		height: 24px;
+		/* font: 22px normal Arial, Helvetica, sans-serif; */
+		font-style: italic;
+		color: #777;
+		border: 1px solid #c5c5c5;
+		-webkit-border-radius: 3px;
+		-moz-border-radius: 3px;
+		border-radius: 3px;
+		padding: 2px 0 5px 10px;
+	}
+	.btn {
+		height: 24px;
+		width: 70px;
+	}
+</style>
 <div class="top_menu">
 	<form method="post" action="" id="fast_search_form">
 		<ul class="top_ul">
-			<li>
-				<select name="year">
+			<li class="top_li">
+				<select name="year" class="select-box">
 					<option value="all">Не имеет значения</option>
 					<?php
 					$max_y = date('y');
@@ -16,7 +63,38 @@
 					?>
 				</select>
 			</li>
-			<li></li>
+			<li class="top_li">
+				<select name="period" class="select-box">
+					<option value="all">Не имеет значения</option>
+					<option>Зимняя сессия</option>
+					<option>Летняя сессия</option>
+				</select>
+			</li>
+			<li class="top_li">
+				<select name="course" class="select-box">
+					<?php
+					for ($i = 1; $i < 6; ++$i) {
+						echo "<option value=" . $i . ">" . $i . " курс</option>";
+					}
+					?>
+				</select>
+			</li>
+			<li class="top_li">
+				<select name="group" class="select-box">
+					<option></option>
+				</select>
+			</li>
+			<li class="top_li">
+				<input type="text" class="inp_search" size="40" name="student" value="" placeholder="Ф.И.О. студента" />
+			</li>
+			<li class="top_li">
+				<input type="submit" name="search" value="Поиск" class="btn" />
+			</li>
+			<li class="top_li">
+				<a href="#" class="sign_up">
+					<img src="/msp/img/lock.png" class="img lock" />
+				</a>
+			</li>
 		</ul>
 	</form>
 </div>
